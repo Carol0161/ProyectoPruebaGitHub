69af9418d871640adf2e0a4e5e4d6b2920f3ec14
Carol0161 <78099991+Carol0161@users.noreply.github.com>
2024-04-03 10:33:43 +0200

    creo un conflicto

--- src/primero/MiClasePrueba.java
+++ src/primero/MiClasePrueba.java
@@ -3,7 +3,8 @@ package primero;
 public class MiClasePrueba {
 
 	public static void main(String[] args) {
-		System.out.println("Mi primer programa");   
+		System.out.println("Mi primer programa");  
+		System.out.println("Mi primer programa 2222"); 
 
 	}
 
