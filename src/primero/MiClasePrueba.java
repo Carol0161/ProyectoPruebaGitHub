5975f9e1120f1c77e3ea01c55648a5cafdcc712b
Carol0161 <78099991+Carol0161@users.noreply.github.com>
2024-04-03 17:47:19 +0200

    COMMIT NUEVO DESDE CASA

--- src/primero/MiClasePrueba.java
+++ src/primero/MiClasePrueba.java
@@ -3,10 +3,11 @@ package primero;
 public class MiClasePrueba {
 
 	public static void main(String[] args) {
+		System.out.println("Mi primer programa");   
+		System.out.println("esta es otra linea de codigo"); 
 		System.out.println("Mi primer programa");  
 		System.out.println("Mi primer programa 2222"); 
 		System.out.println("esta es otra linea de codigo");
-		System.out.println("otra linea mas");
 		System.out.println("otra linea mas");
 	}
 
