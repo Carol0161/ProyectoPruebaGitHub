9a1f372b64ca4cc6e301d03c62dde3a50292e7e3
AulaUCJC <AulaUCJC@192.168.164.103>
2024-04-03 10:29:39 +0200

    aaaa

--- src/primero/MiClasePrueba.java
+++ src/primero/MiClasePrueba.java
@@ -3,7 +3,7 @@ package primero;
 public class MiClasePrueba {
 
 	public static void main(String[] args) {
-		System.out.println("Mi primer programa");
+		System.out.println("Mi primer programa");   
 
 	}
 
